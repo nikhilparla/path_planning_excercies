aed3aad562d38ad67804f18b92214eb693a4bf05
nikhilparla <nikhilp.bits@gmail.com>
2020-01-30 22:00:51 -0500

    expansion_grid completed

---
+++ expansion_search.py
@@ -0,0 +1,121 @@
+# ----------
+# User Instructions:
+#
+# Define a function, search() that returns a list
+# in the form of [optimal path length, row, col]. For
+# the grid shown below, your function should output
+# [11, 4, 5].
+#
+# If there is no valid path from the start point
+# to the goal, your function should return the string
+# 'fail'
+# ----------
+
+# Grid format:
+#   0 = Navigable space
+#   1 = Occupied space
+
+grid = [[0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0]]
+
+expand_grid =  [[-1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1]]
+
+init = [0, 0]
+goal = [len(grid)-1, len(grid[0])-1]
+cost = 1
+
+delta = [[-1, 0],  # go up
+         [0, -1],  # go left
+         [1, 0],  # go down
+         [0, 1]]  # go right
+
+delta_name = ['^', '<', 'v', '>']
+res_list = [init[i] + delta[1][i] for i in range(len(init))]
+#print(res_list)
+#search(grid, init, goal, cost)
+
+
+def search(grid, init, goal, cost):
+    # ----------------------------------------
+    # insert code here
+
+    #init all values in expand_grid to -1
+    #expand_grid = grid[:]
+    #expand_grid = list(grid)
+
+    open = [init]
+    curr = init
+    gvalue = 0
+    visited_cells = []
+    #gvalue_list = [[init], gvalue]
+    gvalue_list = []
+
+#    for _iterx in range(len(expand_grid)):
+#        for _itery in range(len(expand_grid[0])):
+#            expand_grid[_iterx][_itery] = -1
+
+    _iter_exp = 0
+    expand_grid[curr[0]][curr[1]] = _iter_exp
+
+    gvalue_list.append([curr, gvalue])
+    while(curr != goal):
+        neighbor_list = check_next(curr)
+
+        # add the found neighbors to the open list if not alrady visited
+        for i in neighbor_list:
+                if (i not in visited_cells and i not in open and grid[i[0]][i[1]] == 0):
+                    open.append(i)
+                    gvalue_list.append([i, gvalue+1])
+
+        #open.remove([curr,gvalue])
+        gvalue_list.remove([curr, gvalue])
+        visited_cells.append(curr)
+
+        gvalue += 1
+        # get the lowest g-value in the list
+        lowest_g = gvalue
+        for cell in gvalue_list:
+                if cell[1] <= lowest_g:
+                        curr = cell[0]
+                        lowest_g = cell[1]
+        gvalue = lowest_g
+        print(gvalue_list)
+        _iter_exp += 1
+        expand_grid[curr[0]][curr[1]] = _iter_exp
+    # ----------------------------------------
+
+    return [gvalue, curr[0], curr[1]]
+
+
+def check_next(curr):
+    neighbor_list = []
+    # if there is a position to the left
+    if(not((curr[1]-1) < 0)):
+            # append the left element to neighbor list. row remains same
+            neighbor_list.append([(curr[0]), curr[1]-1])
+    # if there is a position to the right
+    if(not((curr[1]+1) > len(grid[0]) - 1)):
+            # append the right element to neighbor list. row remains same
+            neighbor_list.append([(curr[0]), curr[1]+1])
+    # if there is a position to the top
+    if(not((curr[0]-1) < 0)):
+            # append the top element to neighbor list. col remains same
+            neighbor_list.append([(curr[0]-1), curr[1]])
+    # if there is a position to the bottom
+    if(not((curr[0]+1) > len(grid)-1)):
+            # append the bottom element to neighbor list. row remains same
+            neighbor_list.append([(curr[0]+1), curr[1]])
+
+    return neighbor_list
+
+
+search(grid, init, goal, cost)
+for row in expand_grid:
+    print(row)
